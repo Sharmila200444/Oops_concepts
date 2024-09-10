a3d098c34355089a00a33876aa0e5ff6b4c7c1a7
Sharmila <146500660+Sharmila200444@users.noreply.github.com>
2024-09-10 21:16:08 +0530

    Add files via upload

---
+++ Method/Method1.java
@@ -0,0 +1,18 @@
+
+
+
+
+public class Method1 {
+    
+    public static void add(){
+        int a=10; 
+        int b=20;
+        int c=a+b;
+        System.out.print(c);
+    }
+    public static void main(String[] args){
+        add();
+
+    }
+
+}
---
+++ Method/Method2.java
@@ -0,0 +1,14 @@
+public class Method2{
+    public static int add(int a, int b){
+        int c=a+b;
+        return c;
+    }
+
+    public static void main(String[] args){
+        System.out.println(add(10,15));
+        
+        System.out.print("a is printed");
+    }
+
+}
+
